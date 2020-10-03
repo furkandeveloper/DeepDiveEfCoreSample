bb7f56554694b6d541cbb10f93f3458956b98e14
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 16:35:19 +0300

    :hammer: Add value generators

---
+++ src/Sample/Generations/DateTimeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Generations
+{
+    /// <summary>
+    /// Date time generator. Using create and update data parameter.
+    /// </summary>
+    public class DateTimeGenerator : ValueGenerator
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        protected override object NextValue(EntityEntry entry)
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/Sample/Generations/FalseGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Generations
+{
+    /// <summary>
+    /// False generator.
+    /// </summary>
+    public class FalseGenerator : ValueGenerator
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        protected override object NextValue(EntityEntry entry)
+        {
+            return false;
+        }
+    }
+}
