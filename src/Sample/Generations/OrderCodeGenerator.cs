5e07d2ea4eb9d4882ef8493c63038cdbe39f18c2
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 17:25:08 +0300

    :hammer: Add order code generator

---
+++ src/Sample/Generations/OrderCodeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Generations
+{
+    public class OrderCodeGenerator : ValueGenerator
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        protected override object NextValue(EntityEntry entry)
+        {
+            return "MBR" + Guid.NewGuid().ToString();
+        }
+    }
+}
