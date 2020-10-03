64f0e1553db00aad66ef2315e39c7b1513e899d3
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 17:29:07 +0300

    :hammer: Add dbcontext

--- src/Sample/Startup.cs
+++ src/Sample/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Sample.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +15,23 @@ namespace Sample
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddDbContext<SampleDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("ConnectionStrings:SampleDbConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
