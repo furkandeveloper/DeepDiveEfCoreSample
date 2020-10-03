347fc5a2db6464ac74677e8208595e3e050f1676
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 16:29:42 +0300

    :hammer: Add db context

---
+++ src/Sample/Context/SampleDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Context
+{
+    public class SampleDbContext : DbContext
+    {
+        public SampleDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected SampleDbContext()
+        {
+        }
+
+        #region Tables
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public virtual DbSet<Order> Orders { get; set; }
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
