d2d40b7109042ce8eac2b94061f6f5085b322520
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 16:29:09 +0300

    :hammer: Add base entity

---
+++ src/Sample/Entities/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace Sample.Entities
+{
+    /// <summary>
+    /// Base Entity.
+    /// </summary>
+    public class BaseEntity
+    {
+        /// <summary>
+        /// Entity create date.
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// Entity Update date.
+        /// </summary>
+        public DateTime UpdateDate { get; set; }
+
+        /// <summary>
+        /// Entity status flag.
+        /// </summary>
+        public bool IsActive { get; set; } = true;
+    }
+}
