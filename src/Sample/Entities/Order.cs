a7ddc21cbb18226135b1aa946e77e2f5e6a138e6
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 16:29:34 +0300

    :hammer: Add order entity

---
+++ src/Sample/Entities/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Entities
+{
+    /// <summary>
+    /// Order entity.
+    /// </summary>
+    public class Order : BaseEntity
+    {
+        /// <summary>
+        /// pk
+        /// </summary>
+        public Guid OrderId { get; set; }
+
+        /// <summary>
+        /// Order code || order number
+        /// </summary>
+        public string OrderCode { get; set; }
+
+        /// <summary>
+        /// Total price
+        /// </summary>
+        public decimal Total { get; set; }
+
+        /// <summary>
+        /// Is order shipped?
+        /// </summary>
+        public bool IsShipped { get; set; }
+
+        /// <summary>
+        /// relation one to many
+        /// </summary>
+        public virtual Customer Customer { get; set; }
+    }
+}
