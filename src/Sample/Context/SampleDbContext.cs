f63985291f83900220156f6e030840087c241fec
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-03 17:26:07 +0300

    :hammer: Tables relation configured.

--- src/Sample/Context/SampleDbContext.cs
+++ src/Sample/Context/SampleDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sample.Entities;
+using Sample.Generations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,100 @@ namespace Sample.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity
+                    .HasKey(pk => pk.CustomerId);
+
+                entity
+                    .Property(p => p.CustomerId)
+                    .HasValueGenerator<GuidGenerator>()
+                    .ValueGeneratedOnAdd();
+
+                entity
+                    .Property(p => p.Name)
+                    .HasMaxLength(200)
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.Surname)
+                    .HasMaxLength(600)
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.PhoneNumber)
+                    .HasMaxLength(11)
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.IsActive)
+                    .HasValueGenerator<TrueGenerator>()
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.CreateDate)
+                    .HasValueGenerator<DateTimeGenerator>()
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.UpdateDate)
+                    .HasValueGenerator<DateTimeGenerator>()
+                    .ValueGeneratedOnAddOrUpdate().
+                    IsRequired();
+
+                entity
+                    .HasMany(m => m.Orders)
+                    .WithOne(o => o.Customer)
+                    .HasForeignKey(fk => fk.CustomerId);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity
+                    .HasKey(pk => pk.OrderId);
+
+                entity
+                    .Property(p => p.OrderCode)
+                    .HasValueGenerator<OrderCodeGenerator>()
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.Total)
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.IsShipped)
+                    .HasValueGenerator<FalseGenerator>()
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.IsActive)
+                    .HasValueGenerator<TrueGenerator>()
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.CreateDate)
+                    .HasValueGenerator<DateTimeGenerator>()
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+                entity
+                    .Property(p => p.UpdateDate)
+                    .HasValueGenerator<DateTimeGenerator>()
+                    .ValueGeneratedOnAddOrUpdate().
+                    IsRequired();
+
+                entity
+                    .HasOne(o => o.Customer)
+                    .WithMany(m => m.Orders)
+                    .HasForeignKey(fk => fk.CustomerId);
+            });
         }
     }
 }
