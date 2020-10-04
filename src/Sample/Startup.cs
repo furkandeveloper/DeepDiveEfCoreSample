087903b36974d5445078b21d4170bd1191c0206a
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-04 15:03:51 +0300

    :hammer: Add database migrate for startup.cs

--- src/Sample/Startup.cs
+++ src/Sample/Startup.cs
@@ -35,8 +35,9 @@ namespace Sample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,SampleDbContext dbContext)
         {
+            dbContext.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
