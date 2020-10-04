0e09edad85ea522703fe231b9e53f3a97a09215a
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-10-04 19:21:54 +0300

    Add MultipleCustomer endpoint

---
+++ src/Sample/Controllers/DefaultController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample.Context;
+using Sample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly SampleDbContext dbContext;
+
+        public DefaultController(SampleDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetIncludeQueryAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> InsertMultipleCustomerAsync()
+        {
+            List<Customer> customers = new List<Customer>();
+            for (int i = 0; i < 3; i++)
+            {
+                var customer = new Customer()
+                {
+                    Name = i + ". customerName",
+                    Surname = i + ". customerSurname",
+                    PhoneNumber = "xxxxxxxxxxx",
+                };
+                customers.Add(customer);
+            }
+            await dbContext.Customers.AddRangeAsync(customers);
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
