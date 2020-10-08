b71a251b8027edc23f700155e79370bd0a6e6840
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-08 22:09:09 +0300

    :hammer: Add stats entity.

---
+++ src/Sample/Entities/Stats.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Entities
+{
+    /// <summary>
+    /// Stats entity for queries.
+    /// </summary>
+    public class Stats
+    {
+        /// <summary>
+        /// Transaction uniqe id.
+        /// </summary>
+        public Guid TransactionId { get; set; }
+
+        /// <summary>
+        /// Query start date.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Query end date.
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Query type.
+        /// </summary>
+        public QueryType QueryType { get; set; }
+    }
+}
