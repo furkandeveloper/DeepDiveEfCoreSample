8343cda2a3a80c2caff1cf55af6346b86d79cf2e
Furkan GÜNGÖR <furkan.dvlp@gmail.com>
2020-10-08 22:07:43 +0300

    :hammer: Add query type enum.

---
+++ src/Sample/Entities/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Entities
+{
+    /// <summary>
+    /// Query type for stats.
+    /// </summary>
+    public enum QueryType
+    {
+        Select = 1,
+        Insert,
+        Update,
+        Delete
+    }
+}
